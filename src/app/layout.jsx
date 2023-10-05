e9fbecf4b9a35a8a0a536a2ce16fcc381d1ccc50
L1c4NsoDangers <l1c4nsodangers@gmail.com>
2023-10-05 14:17:20 +0700

    update 14

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,7 @@
 import CommonLayout from "@/components/client-view/common-layout";
 import "./globals.css";
-import Inter from "next/font";
+import { Inter } from "next/font/google";
 
-// ...
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
