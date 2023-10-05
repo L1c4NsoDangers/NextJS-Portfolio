08926a4369887ec3893d6e11a77ecb3f1314f5ad
L1c4NsoDangers <l1c4nsodangers@gmail.com>
2023-10-05 13:55:33 +0700

    update 11

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// next.config.js
+module.exports = {
+  // ...
+  experimental: {
+    reactServerComponents: true,
+  },
+  // ...
+};
